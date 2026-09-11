c8325e89774507e731e9c9573d942644716181ba
avishekroyyash <avishekroyyash@gmail.com>
2026-09-11 21:02:05 +0600

    solve task five again

--- answer.js
+++ answer.js
@@ -87,24 +87,29 @@ else{
 
 // task 5
 
-const getChaseVerdict= (target, scored, ballsLeft) =>{
+const getChaseVerdict = (target, scored, ballsLeft) => {
     const runsNeeded = target - scored;
-    const requiredRate = (runsNeeded / ballsLeft) * 6
-    if(runsNeeded<=0){
-        return '"Won"'
-    }
-    else if(ballsLeft<=0 && runsNeeded>0){
-        return '"Lost"'
-    }
-    else if (requiredRate<=6){
-        return `"Need ${runsNeeded} runs in ${ballsLeft} balls | Comfortable"`
-    }
-      else if (requiredRate>6 && requiredRate<=12){
-        return `"Need ${runsNeeded} runs in ${ballsLeft} balls | Tough"`
-    }
-      else if (requiredRate>12){
-        return `"Need ${runsNeeded} runs in ${ballsLeft} balls | Almost Impossible"`
+
+    if (runsNeeded <= 0) {
+        return "Won";
     }
 
-}
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    if (requiredRate <= 6) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Comfortable`;
+    } 
+    else if (requiredRate <= 12) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Tough`;
+    } 
+    else {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Almost Impossible`;
+    }
+};
+
+
 
--- task5.js
+++ task5.js
@@ -1,23 +1,25 @@
-const getChaseVerdict= (target, scored, ballsLeft) =>{
+const getChaseVerdict = (target, scored, ballsLeft) => {
     const runsNeeded = target - scored;
-    const requiredRate = (runsNeeded / ballsLeft) * 6
-    if(runsNeeded<=0){
-        return '"Won"'
-    }
-    else if(ballsLeft<=0 && runsNeeded>0){
-        return '"Lost"'
-    }
-    else if (requiredRate<=6){
-        return `"Need ${runsNeeded} runs in ${ballsLeft} balls | Comfortable"`
-    }
-      else if (requiredRate>6 && requiredRate<=12){
-        return `"Need ${runsNeeded} runs in ${ballsLeft} balls | Tough"`
-    }
-      else if (requiredRate>12){
-        return `"Need ${runsNeeded} runs in ${ballsLeft} balls | Almost Impossible"`
+    if (runsNeeded <= 0) {
+        return "Won";
     }
 
-}
+    if (ballsLeft <= 0) {
+        return "Lost";
+    }
 
-const result = getChaseVerdict(150,149,1)
+    const requiredRate = (runsNeeded / ballsLeft) * 6;
+
+    if (requiredRate <= 6) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Comfortable`;
+    } 
+    else if (requiredRate <= 12) {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Tough`;
+    } 
+    else {
+        return `Need ${runsNeeded} runs in ${ballsLeft} balls | Almost Impossible`;
+    }
+};
+
+const result = getChaseVerdict(150, 149, 1);
 console.log(result);
